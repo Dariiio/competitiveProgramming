e1331dd4116e3bb40d5b75da1499fccaab68cfeb
Darío <neocubo1@gmail.com>
2018-10-04 13:41:50 -0300

    Agrego makefile

---
+++ CodeForces/div3-479/B-twoGramAC.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define pb push_back
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define dprint(v) cout << #v"=" << v << endl //;)
+#define mkp make_pair
+#define dpr(v) cout << #v"=" << v << endl //;)
+#define dpra(a,n) { forn(i,(n)) cout << (a)[i] << (i==(n)-1?'\n':' '); }
+#define dprv(vec) dpra(vec,sz(vec))
+
+int n;
+string s,ss;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin>>n>>s;
+	for(int i=0;i<n-1;i++){
+		ss+=(s[i]+s[i+1]);
+		cout<<ss<<" ";
+	}
+	
+	return 0;
+}
---
+++ Makefile
@@ -0,0 +1,5 @@
+hola:
+	echo holis
+clean:
+	find . -type f -name '*.o' -delete 
+	find . -type f -executable -delete
